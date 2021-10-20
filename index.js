edcf6b807652792a37d82f0926c0f665617f1f09
Jon Kimmel <jonkimmel16@gmail.com>
2021-10-20 12:57:03 -0700

    update

--- index.js
+++ index.js
@@ -1,75 +1,64 @@
 //quiz questions
-const questions= [
-    //key 0 skip
-    {
-        question: 'skip',
-        answers:[
-            'skip'
-        ],
-        correctAnswer: 'skip'
-    },
+const questions = [
+	//key 0 skip
+	{
+		question: "skip",
+		answers: ["skip"],
+		correctAnswer: "skip",
+	},
 
-    //question 1
-    {
-        question: 'When did the United States declare independence?',
-        answers:[
-            'July 4, 1776',
-            'July 4, 1775',
-            'July 3, 1777',
-            'July 4, 1783'
-        ],
-        correctAnswer: 'July 4, 1776'
-    },
+	//question 1
+	{
+		question: "When did the United States declare independence?",
+		answers: ["July 4, 1776", "July 4, 1775", "July 3, 1777", "July 4, 1783"],
+		correctAnswer: "July 4, 1776",
+	},
 
-    //question 2
-    {
-        question: 'Who was part of the Committee of Five that drafted the Declaration of Independence?',
-        answers:[
-            'John Adams, Thomas Jefferson, Benjamin Franklin, George Washington, James Madison',
-            'Roger Sherman, Benjamin Franklin, John Hancock, John Jay, George Washington',
-            'Thomas Jefferson, John Adams, Roger Sherman, Robert Livingston, Benjamin Franklin',
-            'Benjamin Franklin, Thomas Paine, John Jay, Thomas Jefferson, James Madison'
-        ],
-        correctAnswer: 'Thomas Jefferson, John Adams, Roger Sherman, Robert Livingston, Benjamin Franklin'
-    },
+	//question 2
+	{
+		question:
+			"Who was part of the Committee of Five that drafted the Declaration of Independence?",
+		answers: [
+			"John Adams, Thomas Jefferson, Benjamin Franklin, George Washington, James Madison",
+			"Roger Sherman, Benjamin Franklin, John Hancock, John Jay, George Washington",
+			"Thomas Jefferson, John Adams, Roger Sherman, Robert Livingston, Benjamin Franklin",
+			"Benjamin Franklin, Thomas Paine, John Jay, Thomas Jefferson, James Madison",
+		],
+		correctAnswer:
+			"Thomas Jefferson, John Adams, Roger Sherman, Robert Livingston, Benjamin Franklin",
+	},
 
-    //question 3
-    {
-        question: 'How many presidents of the United States are there?',
-        answers:[
-            '44',
-            '48',
-            '45',
-            '46'
-        ],
-        correctAnswer: '45'
-    },
+	//question 3
+	{
+		question: "How many presidents of the United States are there?",
+		answers: ["44", "48", "45", "46"],
+		correctAnswer: "46",
+	},
 
-    //question 4
-    {
-        question: 'When was the U.S Marine Corps created?',
-        answers:[
-            'September 18, 1947',
-            'July 4, 1775',
-            'October 13, 1777',
-            'November 10, 1775'
-        ],
-        correctAnswer: 'November 10, 1775'
-    },
+	//question 4
+	{
+		question: "When was the U.S Marine Corps created?",
+		answers: [
+			"September 18, 1947",
+			"July 4, 1775",
+			"October 13, 1777",
+			"November 10, 1775",
+		],
+		correctAnswer: "November 10, 1775",
+	},
 
-    //question 5
-    {
-        question: 'Which U.S. President is in the wrestling hall of fame?',
-        answers:[
-            'Teddy Roosevelt',
-            'George Washington',
-            'Abraham Lincoln',
-            'Warren Harding'
-        ],
-        correctAnswer: 'Abraham Lincoln'
-    }
-
-]
+	//question 5
+	{
+		question: "Which U.S. President is in the wrestling hall of fame?",
+		answers: [
+			"Teddy Roosevelt",
+			"George Washington",
+			"Abraham Lincoln",
+			"Warren Harding",
+		],
+		correctAnswer: "Abraham Lincoln",
+	},
+];
 
 
 //set global vars for question counter
